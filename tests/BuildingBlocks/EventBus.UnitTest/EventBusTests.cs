ee639d11ec4943097762a37d30a3643ae1239543
Parviz Rovshan Aliyev <44087592+parvizrovshanaliyev@users.noreply.github.com>
2023-08-19 12:34:32 +0400

    change sdk version from 5 to 6

---
+++ tests/BuildingBlocks/EventBus.UnitTest/EventBusTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+
+namespace EventBus.UnitTest
+{
+    [TestClass]
+    public class EventBusTests
+    {
+        private ServiceCollection _services;
+        private ILogger _logger;
+        public EventBusTests()
+        {
+            _services=new ServiceCollection();
+
+            _services.AddLogging();
+        }
+    }
+}
